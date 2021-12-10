6e08bc4ca83b4c254251f826b1b10c881753da94
Abbotton <uctoo@foxmail.com>
2021-12-10 12:29:25 +0800

    :sparkles: 添加主控端租户管理菜单

---
+++ app/Console/Commands/SaasInit.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Dcat\Admin\Models\Menu;
+use Illuminate\Console\Command;
+
+class SaasInit extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'saas:init';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'SAAS后台初始化';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $createdAt = Carbon::now();
+        Menu::insert([
+            [
+                'parent_id' => 0,
+                'order' => 8,
+                'title' => '租户管理',
+                'icon' => 'fa-cube',
+                'uri' => '/tenant',
+                'created_at' => $createdAt,
+            ],
+            [
+                'parent_id' => 0,
+                'order' => 9,
+                'title' => '域名管理',
+                'icon' => 'fa-internet-explorer',
+                'uri' => '/domain',
+                'created_at' => $createdAt,
+            ]
+        ]);
+    }
+}
